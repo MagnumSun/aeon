b2c4eeb0f6fe7309d8bfebf2ecc552cdb1c012d1
Austin Magnuson <austinmagnuson@Austins-MacBook-Pro.local>
2016-09-13 16:50:05 -0700

    i

---
+++ js/index 6.js
@@ -0,0 +1,106 @@
+setTimeout(function(){
+        $(".element0").typed({
+            strings: [""],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 0);
+
+setTimeout(function(){
+        $(".element1").css("display", "inherit");
+        $(".element1").typed({
+            strings: ["Welcome to Austin Magnuson's personal"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 2000);
+
+setTimeout(function(){
+        $(".element2").css("display", "inherit");
+        $(".element2").typed({
+            strings: ["website. My name is .'. ǽ .'. which roughly"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 4000);
+
+setTimeout(function(){
+        $(".element3").css("display", "inherit");
+        $(".element3").typed({
+            strings: ["translates to Aeon Tenebris."],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 6000);
+
+setTimeout(function(){
+        $(".element4").css("display", "inherit");
+        $(".element4").typed({
+            strings: ["I am from the 7th dimension and was"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 8000);
+
+setTimeout(function(){
+        $(".element5").css("display", "inherit");
+        $(".element5").typed({
+            strings: ["brought into this world by Austin to build"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 10000);
+    
+setTimeout(function(){
+        $(".element6").css("display", "inherit");
+        $(".element6").typed({
+            strings: ["psychic technologies."],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 12000);
+    
+setTimeout(function(){
+        $(".element7").css("display", "inherit");
+        $(".element7").typed({
+            strings: ["Updates:"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 14000);
+    
+setTimeout(function(){
+        $(".element8").css("display", "inherit");
+        $(".element8").typed({
+            strings: ["Psytech"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 16000);
